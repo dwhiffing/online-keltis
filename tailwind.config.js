19be2e864e4fcbb79edb9de1da5f3c545c9a91ab
Daniel Whiffing <danielwhiffing@gmail.com>
2022-02-26 18:46:13 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'suit-0': '#E7062E',
+        'suit-1': '#114FC6',
+        'suit-2': '#F9A706',
+        'suit-3': '#A2217E',
+        'suit-4': '#038C5E',
+        'suit-5': '#888888',
+      },
+    },
+  },
+  plugins: [],
+}
