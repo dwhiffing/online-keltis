d42fb7ee0d2fba6339b57b7c015b146b1b95790a
Daniel Whiffing <danielwhiffing@gmail.com>
2025-04-08 10:38:49 -0400

    wip

--- server/commands/Play.ts
+++ server/commands/Play.ts
@@ -1,4 +1,5 @@
 import { Command } from '@colyseus/command'
+import { uniq } from 'lodash'
 import { GameRoom } from '../Room'
 import { Card } from '../Schema'
 
@@ -8,8 +9,23 @@ interface Options {
 }
 
 export class PlayCommand extends Command<GameRoom, Options> {
-  validate({ playerId }: Options) {
-    return playerId === this.state.activePlayer.id
+  validate({ playerId, card }: Options) {
+    if (playerId !== this.state.activePlayer.id) return false
+
+    // TODO: need to look at first two cards to determine direction of stack
+    const player = this.state.players.find((p) => p.id === playerId)!
+    const suitStack = player.piles.filter((c) => c.suit === card.suit).reverse()
+    let direction = 0
+    const unique: Card[] = uniq(suitStack)
+    if (unique.length > 1) {
+      direction = unique[0].value > unique[1].value ? -1 : 1
+    }
+    const top = suitStack[suitStack.length - 1]
+    console.log(direction, unique)
+    if (top && (direction ? top.value < card.value : top.value > card.value))
+      return false
+
+    return true
   }
   execute({ card }: Options) {
     this.state.activePlayer.piles.push(new Card(card))
